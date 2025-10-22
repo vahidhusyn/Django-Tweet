0e111323c716cd5082bf1a2a3c35ecc3151176a7
Vahid <vahidhusyn@gmail.com>
2025-10-22 10:08:30 +0530

    added tailwind STATICDIRS

--- djangotweet/djangotweet/settings.py
+++ djangotweet/djangotweet/settings.py
@@ -190,10 +190,14 @@ CRISPY_TEMPLATE_PACK = "tailwind"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
 
 STATIC_URL = '/static/'
-STATIC_ROOT = [os.path.join(BASE_DIR, 'theme/static')]
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # ✅ single folder for collected static files
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'theme' / 'static',  # ✅ your Tailwind build output
+]
 
 LOGIN_URL = '/accounts/login'
 LOGIN_REDIRECT_URL = '/tweet'
